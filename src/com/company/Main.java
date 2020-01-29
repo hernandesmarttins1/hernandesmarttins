dc5f40d29a7ab0b37cf53fe365acf140394917e1
hernandesmarttins <hernandes-martins1@outlook.com>
2020-01-28 22:55:49 -0300

    criacao do projeto

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.
+public class Main {
+
+    public static void main(String[] args) {
+        double num_funcionario = 1548965;
+        double horas_trampa;
+        double valor_recebido;
+        double salarioFuncionario;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("numero do funcionario: %f", num_funcionario);
+        System.out.println("Horas trabalhadas: 157945");
+    }
+}
